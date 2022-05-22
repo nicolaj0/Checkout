ed3eefb6399ea382e4fa26554a53fd82557203f3
Julien <julien.nicolas@gopigment.com>
2022-05-22 08:09:52 +0000

    ic

---
+++ Checkout/Program.cs
@@ -0,0 +1,35 @@
+using Checkout;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
+using ServiceUtils;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerDocument();
+//builder.Services.AddSwagger("Checkout", "v1");
+var app = builder.Build();
+//app.UseSwaggerEndpointAndWebUI("v1", "Checkout", "v1");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
